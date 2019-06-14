87070aa1fd2b84c7da67728f535cf2470416c8fe
mohamed-saad <mahameds19999@gmail.com>
2019-06-14 20:37:07 +0200

    add the slide up and down to the nav list

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// slide the navbar list on clicking the collapse buton
+let collapseButton = document.querySelector('.collapse-btn');
+let isCollapsed = true;
+// slide the list of the navbar
+collapseButton.addEventListener('click', function() {
+    // toggle the list
+    isCollapsed = !isCollapsed;
+    
+    let collapseList = document.querySelector('.nav-list-section');
+    
+    const originHeight = '210px';
+    collapseList.style.transition = 'height 0.3s';
+    collapseList.style.height = originHeight;
+    // check the current state of the list
+    if (!isCollapsed) {
+        // slide the list down
+        collapseList.classList.remove('visible-bg');
+        collapseList.style.height = originHeight;
+    } else {
+        
+        // slide the list up
+        collapseList.style.height = '0';
+        // add the class after sliding up for big screens
+        setTimeout(() => {
+            collapseList.classList.add('visible-bg');
+        }, 300);
+    }
+})
